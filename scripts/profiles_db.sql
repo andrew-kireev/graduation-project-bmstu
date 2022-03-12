1d963d379022db427a168c9f08885c1091e3e000
Andrey Kireev <asekireev@avito.ru>
2022-03-12 19:57:22 +0300

    Сервер для админки

--- scripts/profiles_db.sql
+++ scripts/profiles_db.sql
@@ -7,5 +7,16 @@ CREATE TABLE IF NOT EXISTS Profiles
     second_name        varchar   not null,
     encrypted_password varchar   not null,
     avatar             varchar   not null,
-    favorite_genre     text[] NOT NULL DEFAULT '{}'::text[]
+    favorite_genre     text[]    NOT NULL DEFAULT '{}'::text[]
 );
+
+CREATE TABLE IF NOT EXISTS admin_profiles
+(
+    profile_id         bigserial not null primary key,
+    login              varchar   not null unique,
+    encrypted_password varchar   not null
+);
+
+
+create index profiles_nickname on profiles using btree (nickname);
+create index admin_profiles_login on admin_profiles using btree (login);
